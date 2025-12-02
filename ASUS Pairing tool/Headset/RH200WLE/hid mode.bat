ac6cb479a76e49ac581891a2b86759c875f84e5c
gͥhoͣsͫt <55955972+GG-GhostGaming@users.noreply.github.com>
2025-12-02 05:32:22 -0500

    Delete ASUS Pairing tool/Headset/RH200WLE directory

--- ASUS Pairing tool/Headset/RH200WLE/hid mode.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-start RH200WLE_FW_Update_tool.exe hidmode     || (echo Error:RH200WLE_FW_Update_tool & goto err)
-echo Success:update done! & exit /b
-:err
-pause
--- ASUS Pairing tool/Headset/RH200WLE/ig update mode.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-start RH200WLE_FW_Update_tool.exe ig_update     || (echo Error:RH200WLE_FW_Update_tool & goto err)
-echo Success:update done! & exit /b
-:err
-pause
